772a23e9c3a86b950a3f8ff0f154341762b91206
Arnav Dewan <57629464+arnxv0@users.noreply.github.com>
2022-02-04 20:50:34 +0530

    Complete URL shortner

--- url-shortner/main/main.go
+++ url-shortner/main/main.go
@@ -15,6 +15,7 @@ func main() {
 		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
 		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
 	}
+
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
 	// Build the YAMLHandler using the mapHandler as the
@@ -29,8 +30,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
+	fmt.Println("Starting the server on localhost:8080")
+	http.ListenAndServe("localhost:8080", yamlHandler)
 }
 
 func defaultMux() *http.ServeMux {
