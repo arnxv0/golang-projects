041a70bdc095f564c287ee3d4a1f1a4392b82569
Arnav Dewan <57629464+arnxv0@users.noreply.github.com>
2022-03-12 18:21:27 +0530

    Complete cyoa

--- cyoa/cmd/cyoaweb/main.go
+++ cyoa/cmd/cyoaweb/main.go
@@ -1,18 +1,83 @@
 // go build cmd/cyoaweb/main.go
 // go run cmd/cyoaweb/main.go
 // go run cmd/cyoaweb/main.go --help
+// godoc -http :3030
 
 package main
 
 import (
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/arnxv0/golang-projects/cyoa"
 )
+
+var storyTmpl = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Choose Your Own Adventure</title>
+  </head>
+  <body>
+    <section class="page">
+      <h1>{{.Title}}</h1>
+      {{range .Paragraphs}}
+        <p>{{.}}</p>
+      {{end}}
+      <ul>
+      {{range .Options}}
+        <li><a href="/story/{{.Chapter}}">{{.Text}}</a></li>
+      {{end}}
+      </ul>
+    </section>
+    <style>
+      body {
+        font-family: helvetica, arial;
+      }
+      h1 {
+        text-align:center;
+        position:relative;
+      }
+      .page {
+        width: 80%;
+        max-width: 500px;
+        margin: auto;
+        margin-top: 40px;
+        margin-bottom: 40px;
+        padding: 80px;
+        background: #FCF6FC;
+        border: 1px solid #eee;
+        box-shadow: 0 10px 6px -6px #797;
+      }
+      ul {
+        border-top: 1px dotted #ccc;
+        padding: 10px 0 0 0;
+        -webkit-padding-start: 0;
+      }
+      li {
+        padding-top: 10px;
+      }
+      a,
+      a:visited {
+        text-decoration: underline;
+        color: #555;
+      }
+      a:active,
+      a:hover {
+        color: #222;
+      }
+      p {
+        text-indent: 1em;
+      }
+    </style>
+  </body>
+</html>`
 
 func main() {
 	port := flag.Int("port", 3000, "port to start web server on")
@@ -32,8 +97,22 @@ func main() {
 	}
 
 	// fmt.Printf("%+v\n", story) // %+v prints the struct as a string
+	tpl := template.Must(template.New("").Parse(storyTmpl))
+	h := cyoa.NewHandler(story, cyoa.WithTemplate(tpl), cyoa.WithPathFunc(pathFn))
 
-	h := cyoa.NewHandler(story)
+	mux := http.NewServeMux()
+	mux.Handle("/story/", h)
+
 	fmt.Printf("Starting the server on port: %d\n", *port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), mux))
+}
+
+func pathFn(r *http.Request) string {
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "/story" || path == "/story/" {
+		path = "/story/intro"
+	}
+
+	path = path[len("/story/"):]
+	return path
 }
